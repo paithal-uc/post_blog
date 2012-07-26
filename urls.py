d270ef6dbde8a15d4f131e84a264c7067d52ffe5
prashanth <paithal@mangaloreinfotech.in>
2012-07-26 18:36:53 +0530

    Updated

--- urls.py
+++ urls.py
@@ -10,10 +10,12 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^post_app/$', 'post_app.views.index', name="index"),
     url(r'^post_app/home$', 'post_app.views.home',name="home"),
+    url(r'^post_app/login$', 'post_app.views.login',name="login"),
     url(r'^post_app/viewpost$', 'post_app.views.viewpost',name="viewpost"),
     url(r'^post_app/comment/(?P<post>\d+)/$', 'post_app.views.comment',name="comment"),
     #url(r'^post_app/comment$', 'post_app.views.comment',name="comment"),
     url(r'^post_app/addpost$', 'post_app.views.addpost',name="addpost"),
+    url(r'^post_app/logout$','post_app.views.logout_view',name="logout"),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
